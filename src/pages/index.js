2676e65bab7eb44b127faf1fdeb7ef29fbd1167a
Imran <imrantipu90@yahoo.com>
2023-02-13 22:04:11 +0100

    head . link , and image tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title> Blog Page</title>
+      </Head>
       <h1>Hello next js</h1>
+      <Link href="/blog/blog">
+      Blog
+      </Link>
+       
+       <Image src='/nature.jpg' width={500} height={300}></Image>
     </>
   )
 }
